49ddc2987c1d5b6cb2e4ea02f4e4d1951e62b74d
Sakine Mazlomyar <mazlomyar12@gmail.com>
2024-07-28 11:11:31 +0200

    init of c++

---
+++ Hello-world/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+/* 
+1) pre processor
+2) compliation assambler  
+3) creating machine code 
+4) linking, extrenal or object library 
+ */
+
+
+
+/* 
+
+
+ */
+using namespace std;
+int main(){
+
+    cout << "Hello World" << endl;
+    return 0;
+}

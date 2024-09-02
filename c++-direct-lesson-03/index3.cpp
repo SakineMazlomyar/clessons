43317f72a3dfad27e36daae3fa41d6eadb7c8aae
Sakine Mazlomyar <mazlomyar12@gmail.com>
2024-09-02 21:49:37 +0200

    post increment and preincrement

--- c++-direct-lesson-03/index3.cpp
+++ c++-direct-lesson-03/index3.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Hello world" << endl;
-    int x, y, result;
+    int x, y, result, m, n, result1;
     x = 2; 
     y = 2;
-    result = x++ * y++;
+    result = x++ * y++; // this results 2 * 2 = 4 post increment som är kopia av x och y
     cout << result << endl;
+    cout << x << endl << y << endl;
+
+    m = 3;
+    n = 3;
+    result1 = ++m * ++n; // this results 4 * 4 = 16; post preincrement som är en reference av x och y
+    cout << result1 << endl;
+    cout << m << endl << n << endl;
+
     return 0;
 
 }

ccb5cba0973f6009377473033e6b0d75f36df6d5
Sakine Mazlomyar <mazlomyar12@gmail.com>
2024-09-03 07:58:32 +0200

    lesson 4 and cmath and cstdlib

---
+++ c++-direct-lesson-04/index4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int x = 2;
+    int result = pow(x, 3);
+    cout << result << endl;
+    cout << fmod(3,2) << endl;
+    cout << abs(-80000) << endl;
+    return 0;
+}
